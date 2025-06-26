98c94065f995e0feed4de312cc14cd17afa070e6
Krishna <krishnalatwal7560@gmail.com>
2025-06-26 11:54:05 +0530

    added routes

---
+++ src/controllers/todo.controller.js
@@ -0,0 +1,20 @@
+import { Todo } from "../models/todo.model";
+
+
+// create
+const createTodo = () => {}
+
+
+// read all
+const getTodo = () => {}
+
+
+// read one
+const getTodoById = () => {}
+
+// update
+const updateTodo = () => {}
+
+
+// delete
+const deleteTodo = () => {}
---
+++ src/routes/todo.routes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+    createTodo,
+    getTodo,
+    getTodoById,
+    updateTodo,
+    deleteTodo
+} from "../controllers/todo.controller";
+
+const router = express.Router();
+
+router.post('/', createTodo);
+router.get('/',getTodo);
+router.get('/:id', getTodoById);
+router.put('/:id', updateTodo);
+router.delete("/:id", deleteTodo);
+
+export default router;
